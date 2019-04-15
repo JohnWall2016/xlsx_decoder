aa4045130037580acbd40e6b189e7207457a3088
John Wall <5672618@qq.com>
2019-04-15 22:27:48 +0800

    init

---
+++ example/xlsx_decoder_example.dart
@@ -0,0 +1,6 @@
+import 'package:xlsx_decoder/xlsx_decoder.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/xlsx_decoder_test.dart
@@ -0,0 +1,16 @@
+import 'package:xlsx_decoder/xlsx_decoder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
