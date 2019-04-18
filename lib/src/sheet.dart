25db6e5ed3b004a5aebaeebe1df23b6d0e961479
JohnWall2016 <5672618@qq.com>
2019-04-18 17:06:47 +0800

    update

--- lib/src/sheet.dart
+++ lib/src/sheet.dart
@@ -1,13 +1,43 @@
 import './workbook.dart';
 import './root_element.dart';
+import './nodes.dart';
+import './range.dart';
+import './relationships.dart';
 
-class Sheet {
+class Sheet extends RootElement {
   Workbook _workbook;
-  XmlElement _sheetIdNode;
-  XmlElement _sheetNode, _sheetRelationshipsNode;
+  XmlElement _IdNode;
+  XmlElement _sheetRelationshipsNode;
+
+  int _maxSharedFormulaId = -1;
+
+  Map<String, XmlElement> _mergeCells = {};
+  Map<String, XmlElement> _dataValidations = {};
+  Map<String, XmlElement> _hyperlinks = {};
+
+  Range _autoFilter = null;
+
+  Relationships _relationships;
 
   Sheet(Workbook workbook, XmlElement idNode, XmlElement node,
-      XmlElement relationshipsNode) {
-    if (node == null) {}
+      XmlElement relationshipsNode)
+      : super(node ??
+            (Element('worksheet', {
+              'xmlns':
+                  'http://schemas.openxmlformats.org/spreadsheetml/2006/main',
+              'xmlns:r':
+                  'http://schemas.openxmlformats.org/officeDocument/2006/relationships',
+              'xmlns:mc':
+                  'http://schemas.openxmlformats.org/markup-compatibility/2006',
+              'mc:Ignorable': 'x14ac',
+              'xmlns:x14ac':
+                  'http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac'
+            })
+                  ..children.add(Element('sheetData')))
+                .toXmlNode()) {
+    _workbook = workbook;
+    _IdNode = idNode;
+    
+    _relationships = Relationships(relationshipsNode);
   }
 }
